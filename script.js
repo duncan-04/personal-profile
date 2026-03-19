9604e056bb9e18b4c93c24f536f3417673548ee7
duncan-04 <kgatoduncan04@icloud.com>
2026-03-19 21:37:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function validateForm(){
 
-let name = document.getElementById("name").value;
-let email = document.getElementById("email").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
 
-if(name === "" || email === ""){
-alert("Please fill in all fields");
-return false;
-}
+  if(name === "" || email === ""){
+    alert("Please fill in all fields");
+    return false;
+  }
 
-if(!email.includes("@")){
-alert("Please enter a valid email");
-return false;
-}
+  if(!email.includes("@")){
+    alert("Please enter a valid email");
+    return false;
+  }
 
-alert("Message sent successfully!");
-return true;
-
+  alert("Message sent successfully!");
+  return true;
 }
